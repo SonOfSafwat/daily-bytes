065a355c42f7b0b490667b1be3915a8a7edb687c
abdelrhman safwat <abdelrhman.safwat@gmail.com>
2020-09-18 11:53:41 +0400

    feat(build): feat(build): add repo section to package.json

---
+++ src/reverse-string.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const reverse = str => {
+  const result = '';
+  for (let index = str.length; index >= 0; index--) {
+    result.push(str[index]);
+  }
+};
+
+export default reverse;
