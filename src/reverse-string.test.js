a787f8641a4391b471eafc1783b540829f6670f7
abdelrhman safwat <abdelrhman.safwat@gmail.com>
2020-09-20 12:24:12 +0400

    test(problem): add tests to reverse string problem

--- src/reverse-string.test.js
+++ src/reverse-string.test.js
@@ -1,6 +1,13 @@
-const reverse = require('./reverse-string');
-const { expect, test } = require('packageName');
+import reverse from './reverse-string';
 
 test('Cat”, return “taC', () => {
   expect(reverse('Cat')).toBe('tac');
+});
+
+test('“The Daily Byte”, return "etyB yliaD ehT”', () => {
+  expect(reverse('The Daily Byte')).toBe('etyB yliaD ehT');
+});
+
+test('“civic”, return “civic”', () => {
+  expect(reverse('civic')).toBe('civic');
 });
